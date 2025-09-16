528fb17f1c0ea3779fb3c4009fc12a3469f975f8
Ishanya Kukade <ishanyak12@gmail.com>
2025-09-17 00:03:40 +0530

    Execute file complete

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,6 @@
 import java.io.File;
-import java.util.HashSet;
+import java.io.IOException;
+import java.util.Arrays;
 import java.util.Scanner;
 import java.util.Set;
 
@@ -14,14 +15,14 @@ public class Main {
 
             String input = scanner.nextLine().trim();
 
-            String[] commands = input.split(" ");
+            String[] commands = input.split("\\s+");
 
             switch (commands[0]) {
                 case "echo" -> {
-                    if (input.split(" ").length < 2) {
+                    if (input.split("\\s+").length < 2) {
                         System.out.println();
                     } else {
-                        echo(input.split(" ")[1]);
+                        echo(input.split("\\s+")[1]);
                     }
                 }
 
@@ -29,8 +30,14 @@ public class Main {
 
                 case "exit" -> exit();
 
+                case "test" -> {
+                    String[] pathCommands = System.getenv("PATH").split(File.pathSeparator);
+                    System.out.println(Arrays.toString(pathCommands));
+                }
+
                 default -> {
-                    System.out.printf("%s: command not found%n", input);
+                    // Validation of command is done inside try-catch in executeFile
+                    executeFile(commands);
                 }
             }
         } while (true);
@@ -47,14 +54,37 @@ public class Main {
     public static void getType(String input) {
         if (isInbuiltCommand(input)) {
             System.out.println(input + " is a shell builtin");
-        } else if (isExecutable(input) != null) {
-            System.out.println(isExecutable(input));
+        } else if (getFilePath(input) != null) {
+            System.out.println(input + " is " + getFilePath(input));
         } else {
             System.out.println(input + ": not found");
         }
     }
 
-    public static String isExecutable(String fileName) {
+    public static void executeFile(String[] commands) throws IOException, InterruptedException {
+        String filePath = getFilePath(commands[0]);
+
+        try {
+
+            ProcessBuilder builder = new ProcessBuilder(commands);
+
+            // By default, ProcessBuilder has two default streams: standard output and standard error
+            // This tells it to merge std error to std output stream
+            builder.redirectErrorStream(true);
+
+            Process process = builder.start();
+
+            process.getInputStream().transferTo(System.out);
+
+            process.waitFor();
+
+        } catch (IOException e) {
+            // In case of invalid command like "AsdasDasd"
+            System.out.println(commands[0] + ": command not found");
+        }
+    }
+
+    public static String getFilePath(String fileName) {
         String[] pathCommands = System.getenv("PATH").split(File.pathSeparator);
 
         for (String path : pathCommands) {
@@ -62,7 +92,7 @@ public class Main {
 
             if (file.exists()) {
                 if (file.canExecute())
-                    return fileName + " is " + file.getAbsolutePath();
+                    return file.getAbsolutePath();
             }
         }
 
